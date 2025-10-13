8f5b6c6093b91fdffc0d9a731e112fa20251131f
Phu Vo <u1153426@umail.usq.edu.au>
2025-10-13 15:00:13 +1000

    :hammer: feat: Update problem 226 - BFS

--- src/tree/problem226/Solution.java
+++ src/tree/problem226/Solution.java
@@ -4,6 +4,9 @@ package tree.problem226;
 // https://leetcode.com/problems/invert-binary-tree/
 // Level: Easy
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
     // #1. Recursion
     // Time Complexity: O(n) where n is the number of nodes in the tree
@@ -16,6 +19,25 @@ public class Solution {
 
         invertTree(root.left);
         invertTree(root.right);
+        return root;
+    }
+
+    // #2. Iteration (BFS)
+    // Time Complexity: O(n) where n is the number of nodes in the tree
+    // Space Complexity: O(w) where w is the maximum width of the tree
+    public TreeNode invertTree2(TreeNode root) {
+        if(root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode current = queue.poll();
+            TreeNode currentLeft = current.left;
+            current.left = current.right;
+            current.right = currentLeft;
+
+            if(current.left != null) queue.offer(current.left);
+            if(current.right != null) queue.offer(current.right);
+        }
         return root;
     }
 }
